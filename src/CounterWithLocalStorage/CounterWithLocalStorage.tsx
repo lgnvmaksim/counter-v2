4ef9dc632f844f16f891e70149102585fdcf4ae3
lgnvmaksim <lgnvmaksim@gmail.com>
2023-01-19 16:50:58 +0300

    commit before start stylization

--- src/CounterWithLocalStorage/CounterWithLocalStorage.tsx
+++ src/CounterWithLocalStorage/CounterWithLocalStorage.tsx
@@ -1,4 +1,4 @@
-import React, {ChangeEvent, memo, useCallback, useState} from 'react';
+import React, {ChangeEvent, memo, useCallback, useEffect, useState} from 'react';
 
 type ValueType = {
     minValue: number,
@@ -10,19 +10,19 @@ type ValueType = {
     error: boolean
 }
 
-export const CounterWithLocalStorage = memo (() => {
+export const CounterWithLocalStorage = memo(() => {
     const [value, setValue] = useState<ValueType>({
-        minValue: 0,
-        maxValue: 1,
+        minValue: 1,
+        maxValue: 5,
         minEnteredValue: 0,
         maxEnteredValue: 0,
-        startValue: 0,
+        startValue: null,
         step: 1,
         error: false,
     })
     const [message, setMessage] = useState<string | null>('Нажми на "Жмяк! и узри магию!')
 
-    const onChangeHandler = useCallback ((e: ChangeEvent<HTMLInputElement>) => {
+    const onChangeHandler = useCallback((e: ChangeEvent<HTMLInputElement>) => {
         setValue({
             ...value,
             [e.currentTarget.id + 'Value']: +e.currentTarget.value, startValue: null
@@ -30,7 +30,7 @@ export const CounterWithLocalStorage = memo (() => {
         setMessage('Выбери что-нить и жмякай "Жмяк!"')
     }, [value])
 
-    const onClickSetHandler = useCallback ( () => {
+    const onClickSetHandler = useCallback(() => {
         setValue({
             ...value,
             minEnteredValue: value.minValue,
@@ -38,40 +38,39 @@ export const CounterWithLocalStorage = memo (() => {
             startValue: value.minValue,
         })
         setMessage(null)
-    }, [value] )
+    }, [value])
 
-    const onClickStartHandler = useCallback ( () => {
+    const onClickStartHandler = useCallback(() => {
         if (value.startValue) {
             setValue({
                 ...value,
                 startValue: value.startValue + value.step
             })
         }
-    },[value])
+    }, [value])
 
-    const onClickResetHandler = useCallback (() => {
+    const onClickResetHandler = useCallback(() => {
         if (value.startValue !== null) {
             setValue({...value, startValue: value.minEnteredValue})
         }
-    },[value])
-
-    const checkCorrectValue = useCallback(() => {
-        let checkCorrectValue: boolean = (value.startValue === value.maxValue
-            || Number(value.startValue) > value.maxValue
-            || Number(value.startValue) <= 0
-            || value.maxValue <= 0)
-        checkCorrectValue&& setMessage('Ата-та по рукам! Не может быть так! Верни корректные значения!')
-        return checkCorrectValue
-    },[value, message])
+    }, [value])
 
     let blockButton: boolean = false
 
-    const disabledButtonToStart = useCallback (() => {
+    const checkCorrectValue = () => value.minValue <= 0 || value.maxValue <= 0 || value.minValue >= value.maxValue
+    useEffect(() => {
+        if (checkCorrectValue()) {
+            setMessage('Ата-та по рукам! Введи корректные значения')
+        }
+    }, [value.minValue, value.maxValue])
+
+
+    const disabledButtonToStart = () => {
         if (value.startValue === value.maxEnteredValue) {
             blockButton = true
         }
         return blockButton
-    },[value])
+    }
 
 
     return <div>
@@ -89,8 +88,9 @@ export const CounterWithLocalStorage = memo (() => {
             value={value.maxValue}
             onChange={onChangeHandler}
         />
-        <button onClick={onClickSetHandler}>Жмяк</button>
-        <div>{value.startValue} {message} </div>
+        <button onClick={onClickSetHandler} disabled={checkCorrectValue()}>Жмяк</button>
+        <div>{value.startValue} </div>
+        <div> {message}</div>
 
         <hr/>
         <div>minEnteredValue: {value.minEnteredValue}</div>
